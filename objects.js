d239ba7f5aa2bdd75acf3df43591ac37f5df3aea
Barnabas Nduwak Suku <barnssuku@gmail.com>
2018-12-21 11:19:48 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,1 +1,10 @@
+/**
+ * An Object to hold playlist key value pair of an artist and their songs
+ */
+
 var playlist = {};
+
+function updatePlaylist(playlist, artist, song) {
+  playlist.artist = song;
+  return playlist;
+}
